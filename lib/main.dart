b109f3f59596c09a11b7fde03a3c2f8424d229df
dishangPatel <dishang009@gmail.com>
2020-02-20 11:07:22 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
-
 import 'AdminDashboard.dart';
-
+import 'Login.dart';
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Project Management System',
       theme: ThemeData(
-        primaryColor: Colors.blueGrey[800],
-        fontFamily: 'Roboto',
+        primaryColor: Colors.blue,
+        fontFamily: 'Roboto'
       ),
-      home: AdminDashboard(),
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => Login(),
+        '/adminDashboard' : (context) => AdminDashboard(),
+      },
     );
   }
 }
